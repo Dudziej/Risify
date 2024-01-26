910c891df345cc8929c1bcaa3a91c06a40d630f4
Dudziej <dudzik32@gmail.com>
2024-01-27 00:34:25 +0100

    unit tests in vitest

---
+++ risify-assesment/src/components/ButtonComponent.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, vi } from 'vitest';
+import { mount } from '@vue/test-utils';
+import ButtonComponent from '../components/ButtonComponent.vue';
+
+describe('ButtonComponent', () => {
+  // Test podstawowego renderowania
+  it('renders a button with default props', () => {
+    const wrapper = mount(ButtonComponent);
+    expect(wrapper.element.tagName).toBe('BUTTON');
+  });
+
+  // Test renderowania z tagiem 'a'
+  it('renders an anchor when the "tag" prop is "a"', () => {
+    const wrapper = mount(ButtonComponent, {
+      props: { tag: 'a' }
+    });
+    expect(wrapper.element.tagName).toBe('A');
+  });
+
+  // Test renderowania z tagiem 'router-link'
+  it('renders a router-link when the "tag" prop is "router-link"', () => {
+    const wrapper = mount(ButtonComponent, {
+      props: { tag: 'router-link' }
+    });
+    expect(wrapper.element.tagName).toBe('ROUTER-LINK');
+  });
+
+  // Test reagowania na props "disabled"
+  it('emits a click event when not disabled', async () => {
+    const wrapper = mount(ButtonComponent);
+    await wrapper.trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('click');
+  });
+
+  it('matches the snapshot', () => {
+    const wrapper = mount(ButtonComponent, {
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
